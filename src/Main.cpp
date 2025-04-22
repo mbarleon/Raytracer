46a5fd2c8e64b6c77904739ae1aa5d2d7d92a496
Leorevoir <leo.quinzler@epitech.eu>
2025-04-22 11:58:29 +0200

    add: base CMakeLists.txt

---
+++ src/Main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2025
+** Raytracer
+** File description:
+** Main.cpp
+*/
+
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "salut monde" << std::endl;
+    return 0;
+}

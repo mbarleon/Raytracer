75d2fe49efe55415cefd9c71ec2568d784b5d2d4
Jason KOENIG <jason.koenig@epitech.eu>
2025-05-02 11:28:39 +0200

    [Add] -help handling

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,9 +10,19 @@
 #include "Logger.hpp"
 #include "Macro.hpp"
 #include "Parser/ParseArguments.hpp"
+#include <cstring>
+
+int help()
+{
+    std::cout << "USAGE: ./raytracer <SCENE_FILE>" << std::endl
+        << "    SCENE_FILE: scene configuration" << std::endl;
+    return SUCCESS;
+}
 
 int main(const int argc, char **argv)
 {
+    if (argc == 2 && strcmp(argv[1], "-help") == 0)
+        return help();
     try {
         if (raytracer::parser::parse_arguments(argc, argv)) {
             raytracer::Core core;

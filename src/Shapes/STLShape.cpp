fd4172bcda21194e2140ea773a4443a36720bc07
Mathieu BARLEON <mathieu.barleon@epitech.eu>
2025-05-05 16:33:30 +0200

    Feat: STL Shape

---
+++ src/Shapes/STLShape.cpp
@@ -0,0 +1,111 @@
+/*
+** EPITECH PROJECT, 2025
+** Raytracer
+** File description:
+** STLShape
+*/
+
+#include "Error.hpp"
+#include "STLShape.hpp"
+
+void raytracer::shape::STLShape::_openFile()
+{
+    _file = std::ifstream(_filename, std::ios::binary);
+
+    if (!_file.is_open()) {
+        throw exception::Error("raytracer::shape::STLShape::_openFile", "Could not open file ", std::string(_filename));
+    }
+
+    _file.seekg(80, std::ios::beg);
+    if (!_file) {
+        throw exception::Error("raytracer::shape::STLShape::_openFile", "Could not read from file ", std::string(_filename));
+    }
+}
+
+void raytracer::shape::STLShape::_countTriangles()
+{
+    _file.read(reinterpret_cast<char *>(&_n_triangles), sizeof(uint32_t));
+    _checkRead(sizeof(uint32_t));
+
+    try {
+        _triangles.reserve(_n_triangles);
+    } catch (__attribute__((unused)) const std::bad_alloc &e) {
+        throw exception::Error("raytracer::shape::STLShape::_countTriangles", "Could not allocate memory for ", std::string(_filename));
+    }
+}
+
+void raytracer::shape::STLShape::_getTriangles()
+{
+    for (uint32_t i = 0; i < _n_triangles; ++i) {
+        uint16_t control;
+        _Vertex vecs[4];
+
+        for (auto &vec : vecs) {
+            _file.read(reinterpret_cast<char *>(&vec), sizeof(float) * 3);
+            _checkRead(sizeof(float) * 3);
+        }
+        _triangles.push_back(_Triangle(vecs));
+        _file.read(reinterpret_cast<char *>(&control), sizeof(uint16_t));
+        _checkRead(sizeof(uint16_t));
+    }
+}
+
+void raytracer::shape::STLShape::_checkRead(const std::streamsize size) const
+{
+    if (!_file || _file.gcount() != size) {
+        throw exception::Error("raytracer::shape::STLShape::_checkRead", "Could not read file ",  std::string(_filename));
+    }
+}
+
+constexpr raytracer::shape::STLShape::STLShape(const char *RESTRICT filename): _filename(filename)
+{
+    _openFile();
+    _countTriangles();
+    _getTriangles();
+    _file.close();
+}
+
+bool raytracer::shape::STLShape::_intersectTriangle(const math::Ray &ray, const _Triangle &triangle) noexcept
+{
+    const math::Vector3D A(triangle._v1._x, triangle._v1._y, triangle._v1._z);
+    const math::Vector3D B(triangle._v2._x, triangle._v2._y, triangle._v2._z);
+    const math::Vector3D C(triangle._v3._x, triangle._v3._y, triangle._v3._z);
+
+    if (const math::Vector3D N(triangle._vec._x, triangle._vec._y, triangle._vec._z); ray._dir.dot(N) >= 0.0f) {
+        return false;
+    }
+
+    const math::Vector3D AB = B - A;
+    const math::Vector3D AC = C - A;
+
+    const math::Vector3D h = ray._dir.cross(AC);
+    const float a = AB.dot(h);
+
+    if (a == 0.0f) {
+        return false;
+    }
+
+    const float f = 1.0f / a;
+    const math::Vector3D s = ray._origin - ray._dir;
+    const float u = f * s.dot(h);
+    if (u < 0.0f || u > 1.0f) {
+        return false;
+    }
+
+    const math::Vector3D q = s.cross(AB);
+    if (const float v = f * ray._dir.dot(q); v < 0.0f || u + v > 1.0f) {
+        return false;
+    }
+
+    return f * AC.dot(q) > 0.0f;
+}
+
+bool raytracer::shape::STLShape::intersect(const math::Ray &ray) const noexcept override
+{
+    for (auto &triangle : _triangles) {
+        if (_intersectTriangle(ray, triangle)) {
+            return true;
+        }
+    }
+    return false;
+}
---
+++ src/Shapes/STLShape.hpp
@@ -0,0 +1,47 @@
+/*
+** EPITECH PROJECT, 2025
+** Raytracer
+** File description:
+** STLShape
+*/
+
+#pragma once
+
+#include "Macro.hpp"
+#include "AShape.hpp"
+
+#include <fstream>
+#include <vector>
+
+namespace raytracer::shape {
+class STLShape final: public AShape
+{
+    public:
+        constexpr explicit STLShape(const char *RESTRICT filename);
+
+        [[nodiscard]] bool intersect(const math::Ray &ray) const noexcept override;
+    private:
+        struct _Vertex
+        {
+            float _x, _y, _z;
+        };
+
+        struct _Triangle {
+            _Vertex _vec, _v1, _v2, _v3;
+
+            constexpr explicit _Triangle(const _Vertex vec[4]) :
+                _vec(vec[0]), _v1(vec[1]), _v2(vec[2]), _v3(vec[3]) {}
+        };
+
+        void _openFile();
+        void _getTriangles();
+        void _countTriangles();
+        void _checkRead(std::streamsize size) const;
+        [[nodiscard]] static bool _intersectTriangle(const math::Ray &ray, const _Triangle &triangle) noexcept;
+
+        std::ifstream _file;
+        uint32_t _n_triangles;
+        const char *RESTRICT _filename;
+        std::vector<_Triangle>_triangles;
+};
+}//namespace raytracer::shape

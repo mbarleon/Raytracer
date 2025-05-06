ae0bab9fe1c9ffb9ffe5b20fc63230af407536c1
Leorevoir <leo.quinzler@epitech.eu>
2025-05-06 20:37:02 +0200

    feat: factory is now templates
    Please enter the commit message for your changes. Lines starting

--- include/Macro.hpp
+++ include/Macro.hpp
@@ -32,4 +32,12 @@
 #define DEFAULT_IDENTATION 0
 #define DEFAULT_FIELD_OF_VIEW 70
 
+/**
+* @brief Math Pi / 180.0
+*/
+#define DEGREE_TO_RADIANT 0.017453292519943295
+
+#define MAX_COLOR 255.0
+#define MIN_COLOR 0.0
+
 static constexpr double EPSILON = 1e-6;
